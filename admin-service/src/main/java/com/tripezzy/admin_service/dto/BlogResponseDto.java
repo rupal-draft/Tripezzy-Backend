479621d35ce632465c3e0b76a1ebbba8b5fcb01f
Rupal Paul <rupalpaulgspr@gmail.com>
2025-04-02 14:16:37 +0530

    Adding dtos for Protobufs

---
+++ admin-service/src/main/java/com/tripezzy/admin_service/dto/BlogResponseDto.java
@@ -0,0 +1,70 @@
+package com.tripezzy.admin_service.dto;
+
+public class BlogResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private Long authorId;
+    private String category;
+    private String tag;
+
+    public BlogResponseDto() {
+    }
+
+    public BlogResponseDto(Long id, String title, String content, Long authorId, String category, String tag) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.authorId = authorId;
+        this.category = category;
+        this.tag = tag;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
---
+++ admin-service/src/main/java/com/tripezzy/admin_service/dto/ProductResponseDto.java
@@ -0,0 +1,70 @@
+package com.tripezzy.admin_service.dto;
+
+public class ProductResponseDto {
+    private String name;
+    private String description;
+    private double price;
+    private int stock;
+    private String category;
+    private String imageUrl;
+
+    public ProductResponseDto() {
+    }
+
+    public ProductResponseDto(String name, String description, double price, int stock, String category, String imageUrl) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+        this.category = category;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
